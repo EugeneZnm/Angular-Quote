35a310e81cf409d897b51abcdd19cc795b66dea2
EugeneZnm <eugenenzioki@gmail.com>
2018-08-12 22:13:15 +0300

    editing of README

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Output, Input } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
 // 1 import Quote class
 import {Quote} from '../quote';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-quote',
@@ -14,13 +15,31 @@ export class QuoteComponent implements OnInit {
     new Quote(1, 'what does not kill you', 'Nietzsche', 'Luther')
   ];
 
-   // create addNewQuote function
+  @Input()  Quote: Quote;
+  // 4 emitter object taking a boolean
+  @Output() isComplete = new EventEmitter<boolean>();
+
+  // 5 creating quoteComplete function
+    quoteComplete(complete: boolean) {
+      // 6 calling emit method on isComplete EventEmitter object passing event to parent company
+      this.isComplete.emit(complete);
+    }
+   // 3 create addNewQuote function
   addNewQuote(quote) {
    const quoteLength = this.quote.length;
    quote.id = quoteLength + 1;
    this.quote.push(quote);
   }
+ // 6 deleteQuote function taking boolean value and index of goal
+  deleteQuote(isComplete) {
+    if (isComplete) {
+      const toDelete = confirm(`YOU STILL CANNOT TAKE IT BACK!!'${this.quote[index].saying}`);
 
+      if (toDelete) {
+        this.quote.splice(index, 1);
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit() {
